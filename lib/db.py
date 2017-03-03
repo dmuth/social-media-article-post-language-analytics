ad7285a287a90731e8988b2e5f9188b992281de7
Douglas Muth <dmuth@dmuth.org>
2017-03-02 20:14:58 -0500

    First draft of database wrapper

---
+++ lib/db.py
@@ -0,0 +1,44 @@
+#
+# This class is used as our database wrapper.
+#
+
+import sqlite3
+
+class db():
+
+	conn = ""
+
+	def __init__(self):
+		self.conn = sqlite3.connect('example.db')
+
+
+	def execute(self, query, args = None):
+		if (args):	
+			retval = self.conn.execute(query, args)
+		else:
+			retval = self.conn.execute(query)
+
+		return(retval)
+
+
+	#
+	# Wrapper to create a table if it does not exist
+	#
+	def createTable(self, table, settings):
+
+		#
+		# Query to see if the table already exists.  If it does, stop.
+		#
+		query = "SELECT name from sqlite_master WHERE name = '%s'" % table
+		results = self.conn.execute(query)
+		for row in results:
+			return True
+
+		#
+		# Table doesn't exist?  Go forth and create it!
+		#
+		query = ("CREATE TABLE %s (%s)" % (table, settings))
+		self.conn.execute(query)
+
+
+
